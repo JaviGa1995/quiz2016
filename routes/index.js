fed3f0ad88ceb7b00c3cfb7e01dcef842e441642
JaviGa1995 <javier.gangosoalvarez@gmail.com>
2016-06-04 14:18:49 +0200

    Autores

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', direccion: __dirname});
 });
+
+
+router.get('/author', function (req,res,next) {
+	res.render('author',{nombre : 'Javier Gangoso Alvarez', 
+		foto : 'http://ciqmz5sexle2rutgetukrfbakrnfe6vbbgj27ojpy353cmzvnxnf6ma.ipfs.neocitiesops.net/foto.jpg'})
+})
+
 
 module.exports = router;
